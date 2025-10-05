52f3a7fffcab5bf2b5c6f3083d04b89e3962b40c
Opeyemi Abass "yemi <32185105+yemistar@users.noreply.github.com>
2025-10-04 20:07:35 -0500

    Opt-in PoultryTopBar to ExperimentalMaterial3Api

--- app/src/main/java/com/example/data_collect/ui/home/PoultryTopBar.kt
+++ app/src/main/java/com/example/data_collect/ui/home/PoultryTopBar.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PoultryTopBar(
     onSync: () -> Unit,
